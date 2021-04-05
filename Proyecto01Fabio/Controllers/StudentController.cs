81d6ddf9bfdd7593594b9014134833e420607bb1
Valeria Mejia Cortes <valeria.380172802@ucaldas.edu.co>
2021-04-05 17:11:07 -0500

    Agregar archivos de proyecto.

---
+++ Proyecto01Fabio/Controllers/StudentController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Proyecto01Fabio;
+
+namespace Proyecto01Fabio.Controllers
+{
+    public class StudentController : Controller
+    {
+        private Proyecto01FabioEntities db = new Proyecto01FabioEntities();
+
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View(db.STUDENT.ToList());
+        }
+
+        // GET: Student/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            STUDENT sTUDENT = db.STUDENT.Find(id);
+            if (sTUDENT == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sTUDENT);
+        }
+
+        // GET: Student/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Student/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "idStudent,typeDocumentStudent,documentStudent,codeStudent,nameStudent,lastNameStudent,genderStudent,emailStudent,phoneStudent")] STUDENT sTUDENT)
+        {
+            if (ModelState.IsValid)
+            {
+                db.STUDENT.Add(sTUDENT);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(sTUDENT);
+        }
+
+        // GET: Student/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            STUDENT sTUDENT = db.STUDENT.Find(id);
+            if (sTUDENT == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sTUDENT);
+        }
+
+        // POST: Student/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "idStudent,typeDocumentStudent,documentStudent,codeStudent,nameStudent,lastNameStudent,genderStudent,emailStudent,phoneStudent")] STUDENT sTUDENT)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(sTUDENT).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(sTUDENT);
+        }
+
+        // GET: Student/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            STUDENT sTUDENT = db.STUDENT.Find(id);
+            if (sTUDENT == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sTUDENT);
+        }
+
+        // POST: Student/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            STUDENT sTUDENT = db.STUDENT.Find(id);
+            db.STUDENT.Remove(sTUDENT);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Proyecto01Fabio/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Proyecto01Fabio.MvcApplication" Language="C#" %>
